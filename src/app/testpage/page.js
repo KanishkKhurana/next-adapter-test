02d395a1b8c251709f81b2a663423268710726aa
Kanishk Khurana <kanishkkhurana152@gmail.com>
2024-10-24 11:16:45 -0400

    tests

--- src/app/testpage/page.js
+++ src/app/testpage/page.js
@@ -1,24 +1,24 @@
 import { createClient } from '@supabase/supabase-js';
 export default function TodoList() {
 
-  const addTodo = async (formData) => {
-    'use server';
-    const { value } = formData;
-    console.log(value)
-    console.log(formData)
+//   const addTodo = async (formData) => {
+//     'use server';
+//     const { value } = formData;
+//     console.log(value)
+//     console.log(formData)
 
-    const supabaseUrl = 'URL';
-    const supabaseKey = "key";
-    const supabase = createClient( supabaseUrl, supabaseKey);
-    const todoItem = value;
-    if (!todoItem) {
-      return;
-    }
-    // Save todo item to database
-    const { data, error } = await supabase.from('todos').insert({
-      todo: "vals",
-    });
-  };
+//     const supabaseUrl = 'https://glslxwlwbkpzdxkudnrb.supabase.co';
+//     const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdsc2x4d2x3YmtwemR4a3VkbnJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkyNTE2MjEsImV4cCI6MjA0NDgyNzYyMX0.5m6xqsUJJP7F6ibVVHrWLpg1t4QLW5iirC-kao9dw_Q";
+//     const supabase = createClient( supabaseUrl, supabaseKey);
+//     const todoItem = value;
+//     if (!todoItem) {
+//       return;
+//     }
+//     // Save todo item to database
+//     const { data, error } = await supabase.from('todos').insert({
+//       todo: "vals",
+//     });
+//   };
 
   return (
     <>
